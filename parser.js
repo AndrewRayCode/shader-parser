5f86f1e63b662a33e3ec0ac30b0352f48e0c8f04
Andrew Ray <delvarworld@gmail.com>
2014-02-01 19:22:49 -0800

    working on parser, saving at max call stack problem

--- parser.js
+++ parser.js
@@ -1,29 +1,35 @@
-var string = Parsimmon.string;
-var regex = Parsimmon.regex;
+var _string = Parsimmon.string;
+var _regex = Parsimmon.regex;
 var succeed = Parsimmon.succeed;
 var seq = Parsimmon.seq;
 var lazy = Parsimmon.lazy;
 var optWhitespace = Parsimmon.optWhitespace;
 
-var keywords = ['uniform', 'varying', 'const'];
+var prefixes = ['precision'];
+var precisions = ['lowp', 'mediump', 'highp'];
+var keywords = ['uniform', 'varying', 'const', 'mediump', 'lowp', 'highp'];
 var types = ['int', 'float', 'vec2', 'vec3', 'vec4', 'mat4', 'sampler2D', 'samplerCube', 'ivec3', 'mat4'];
+var infixes = [ '*', '+', '<<', '>>', '<', '>', '<=', '>=', '==', '!=', '&', '^', '|', '&&', '||' ];
+
+var assignmentInfix = [ '=', '+=', '-=', '*=', '/=', '%=', '<<=', '>>=', '&=', '^=', '|=' ];
 
 var fnTypes = ['void'].concat( types );
 
 var json = (function() {
 
-    var json = lazy(function() {
-        return stmtWithFunction.many();
-        //return object
-            //.or(stmt)
-            //.or(array)
-            //.or(stringLiteral)
-            //.or(numberLiteral)
-            //.or(nullLiteral)
-            //.or(trueLiteral)
-            //.or(falseLiteral)
-            //.skip(regex(/^\s*/m));
-    });
+
+
+    var escapeRegex = function( str ) {
+        return (str + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+    };
+
+    var listToRegex = function( list ) {
+        var regex = [];
+        for( var x = 0; x < list.length; x++ ) {
+            regex.push( escapeRegex( list[x] ) );
+        }
+        return new RegExp( '^' + regex.join('|') );
+    };
 
     var escapes = {
         b: '\b',
@@ -33,108 +39,147 @@ var json = (function() {
         t: '\t'
     };
 
-    var stringLiteral = regex(/^"(\\.|.)*?"/).map(function(str) {
-        return str.slice(1, -1).replace(/\\u(\d{4})/, function(_, hex) {
-        return String.fromCharCode(parseInt(hex, 16));
-        }).replace(/\\(.)/, function(_, ch) {
-        return escapes.hasOwnProperty(ch) ? escapes[ch] : ch;
-        });
+    //var array = seq([regex(/^\[\s*/m), commaSep(json), string(']')]).map(function(results) {
+        //return results[1];
+    //});
+
+    //var pair = seq([stringLiteral.skip(regex(/^\s*:\s*/m)), json]);
+
+    //var object = seq([regex(/^[{]\s*/m), commaSep(pair), string('}')]).map(function(results) {
+        //var pairs = results[1];
+        //var out = {};
+        //for (var i = pairs.length-1; i >= 0; i -= 1) {
+        //out[pairs[i][0]] = pairs[i][1];
+        //}
+        //return out;
+    //});
+
+//var types = ['int' ,'float' ,'vec2' ,'vec3' ,'vec4' ,'vec3' ,'mat4' ,'sampler2D' ,'samplerCube' ,'int' ,'ivec3' ,'float' ,'vec3' ,'vec2' ,'vec3' ,'vec4' ,'mat4' ,'sampler2D'];
+    //
+    //var nullLiteral = string('null').result(null);
+    //var trueLiteral = string('true').result(true);
+    //var falseLiteral = string('false').result(false);
+
+    // todo: better way to not include semicolon?
+    var statement = lazy(function() {
+        return seq([
+            assignment
+                .or(functionDecleration)
+                .or(invocation)
+                .or(decleration)
+                .or(expression)
+                .skip( string(';').or( Parsimmon.eof ) )
+        ]);
     });
 
-    var integerLiteral = regex(/^\d+/i).map(parseInt);
+    var expression = lazy(function() {
+        return integer
+            .or( infixed )
+            .or( number )
+            .or( invocation )
+            .or( varName )
+            .or( grouped );
+    });
+
+    //
+    // Language agnostic helper functions
+    //
+    var between = function( left, right, parser ) {
+        return left.then( parser ).skip( right );
+    };
+
+    var parenthed = function( parser ) {
+        return between(string('('), string(')'), parser);
+    };
+
+    var commaSep = function(parser) {
+        var commaParser = ws( string( ',') ).then(parser).many();
+        return seq([parser, commaParser]).map(function(results) {
+            return [results[0]].concat(results[1]);
+        }).or(succeed([]));
+    };
+
+    // Ignore whitespace
+    var ws = function( f ) {
+        return Parsimmon.optWhitespace.then( f );
+    };
+
+    // Can this be done better?
+    var string = function(s) {
+        return ws( _string(s) );
+    };
+    var regex = function(r) {
+        if( r instanceof Array ) {
+            r = listToRegex( r );
+        }
+        return ws( _regex(r) );
+    };
+
+    //
+    // Terminators
+    //
+
+    var integer = regex(/^\d+/i).map(parseInt);
+    var number = regex(/^\d+(([.]|e[+-]?)\d+)?/i).map(parseFloat);
+    var stringLiteral = regex(/^"(\\.|.)*?"/).map(function(str) {
+        return str.slice(1, -1);
+    });
 
     var varName = regex(/^[a-z_][a-z0-9_]*/i).map(function(str) {
         // todo: reserved words, scope test
         return str;
-    }).skip(optWhitespace);
-
-    var numberLiteral = regex(/^\d+(([.]|e[+-]?)\d+)?/i).map(parseFloat);
-
-    function commaSep(parser) {
-        var commaParser = regex(/^,\s*/m).then(parser).many();
-        return seq([parser, commaParser]).map(function(results) {
-            return [results[0]].concat(results[1]);
-        }).or(succeed([]));
-    }
-
-    var array = seq([regex(/^\[\s*/m), commaSep(json), string(']')]).map(function(results) {
-        return results[1];
     });
 
-    var pair = seq([stringLiteral.skip(regex(/^\s*:\s*/m)), json]);
-
-    var object = seq([regex(/^[{]\s*/m), commaSep(pair), string('}')]).map(function(results) {
-        var pairs = results[1];
-        var out = {};
-        for (var i = pairs.length-1; i >= 0; i -= 1) {
-        out[pairs[i][0]] = pairs[i][1];
-        }
-        return out;
-    });
-
-//var types = ['int' ,'float' ,'vec2' ,'vec3' ,'vec4' ,'vec3' ,'mat4' ,'sampler2D' ,'samplerCube' ,'int' ,'ivec3' ,'float' ,'vec3' ,'vec2' ,'vec3' ,'vec4' ,'mat4' ,'sampler2D'];
     //
-    var nullLiteral = string('null').result(null);
-    var trueLiteral = string('true').result(true);
-    var falseLiteral = string('false').result(false);
+    // Constructs
+    //
 
-    var expression = integerLiteral;
+    var infix = regex( infixes );
+    var type = regex( types );
+    var preVarWords = regex( [].concat( prefixes, precisions, keywords ) );
 
-    var type = regex(new RegExp('^' + types.join('|')));
-
-    // float num
+    // [mediump uniform] float num
     var decleration = seq([
-        type.skip(optWhitespace), varName
+        preVarWords.many(), type, varName
     ]);
     
     // float num = expression
     var assignment = seq([
         decleration,
-        string('=').skip(optWhitespace),
+        regex( assignmentInfix ),
         expression
     ]);
 
-    // float num[ = expression]
-    var varStatement = assignment.or( decleration );
+    var commaList = string('(').then(function() {
+        return expression.commaStuff().skip(string(')'));
+    });
 
-    // uniform float num[ = expression]
-    var headVarStatement = seq([
-        regex(new RegExp(keywords.join('|'))).skip(optWhitespace),
-        varStatement
-    ]);
-
-    var statementBody =
-        assignment
-        .or(decleration)
-        .or(varStatement)
-        .or(headVarStatement)
-        .or(expression);
-
-    // todo: better way to not include semicolon?
-    var stmt = seq([ statementBody.skip(regex(/\s*;\s*/m)) ]);
-
-    var args = string('(').skip(optWhitespace).then(function() {
+    var args = string('(').then(function() {
         return commaSep( varName ).skip(string(')'));
     });
 
-    var block = seq([
-        regex(/^[{]\s*/m),
-        stmt.many().or(succeed([])),
-        string('}')
-    ]).map(function(results) {
-        return results[1];
-    });
+    var block = between( string( '{' ), string( '}' ), statement.many() );
 
-    var fn = seq([
-        regex(new RegExp(fnTypes.join('|'))).skip(optWhitespace),
-        varName.skip(optWhitespace),
-        args.skip(optWhitespace),
+    var functionDecleration = seq([
+        regex( fnTypes ),
+        varName,
+        args,
         block
     ]);
 
-    var stmtWithFunction = stmt.or(fn);
+    var invocation = seq([
+        varName.or( type ),
+        parenthed( commaSep( expression ) )
+    ]);
 
-    return json;
+    var grouped = parenthed( expression );
+
+    var infixed = seq([ varName, infix, varName ]);
+
+    return lazy(function() {
+        return statement.many();
+    });
+
 })();
 
 var str1 = "\
@@ -145,11 +190,11 @@ uniform float p = 1;\n\
 varying float intensity;\
 \
 void main() {\
-    vec3 q = 1;\
+    vec4 glow = a + b;\
 }";
 
-var str2 = "vec3 q = 1;";
-var result = json.parse( str1 );
+var str2 = "a + b;";
+var result = json.parse( str2 );
 /*
     vec3 vNormal = normalize( normalMatrix * normal );\
     vec3 vNormel = normalize( normalMatrix * viewVector );\
